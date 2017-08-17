e21b0b992d9d3517d5724e0005aab0d598a5be12
nakkat <nakkat@outlook.com>
2017-08-17 09:33:12 +0900

    Add softmax activation function

--- pydeep/activations.py
+++ pydeep/activations.py
@@ -11,3 +11,10 @@ def relu(x):
 
 def linear(x):
     return x
+
+
+def softmax(x):
+    c = np.max(x)
+    exp_x = np.exp(x - c)   # prevent overflow
+    sum_exp_x = np.sum(exp_x)
+    return exp_x / sum_exp_x
